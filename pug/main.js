cd45529693b302c1731ffe310cbfa2ea543db208
ivan caro <ivanfelipecaro98@gmail.com>
2022-05-16 20:16:41 -0500

    Consigna con pug

---
+++ pug/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { webRouter } = require('./router/webRouter.js')
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// const handlebarsConfig = {
+//     defaultLayout: 'main.pug',
+//     layoutsDir: __dirname + '/views/layouts'
+//   }
+/* ------------------------------------------------------ */
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.use(webRouter)
+
+/* ------------------------------------------------------ */
+/* Server Listen */
+
+const PORT = 8080
+const server = app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+});
+server.on('error',error => console.log(`Error en el servidor ${error}`))
